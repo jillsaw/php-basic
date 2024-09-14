6339f4c40c432876eedd921502ed7b07f0f0894e
jillsaw <uiharu4510471@gmail.com>
2024-09-14 18:44:45 +0900

    kadai_005

---
+++ index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="ja">
+ <head>
+   <meta charset="utf-8">
+   <title>PHPの基礎</title>
+ </head>
+ <body>
+   <p>この文章はHTMLで出力しています。</p>
+   <p>
+     <?php
+      echo 'こんにちは！';
+      //1行の場合
+      /*
+      これは
+      複数行で
+      書く場合です*/
+     ?>
+   </p>
+ </body>
+</html>
